6d2761980d3ccacafe2f688ee9b886579b3664e3
fedebol <fedeboldrini@gamil.com>
2022-10-28 08:11:29 -0300

    Prueba

--- Models/Especialidad.cs
+++ Models/Especialidad.cs
@@ -1,9 +1,15 @@
-﻿namespace Turnos.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Turnos.Models
 {
 
 	public class Especialidad
 	{
+        [Key]
 		public int IdEspecialidad { get; set; }
+
+
+		public string Descripcion { get; set; }
 		
 	}
 }
